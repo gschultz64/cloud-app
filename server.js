98f492627ea201899c3b3fcb30065b3145ef00bf
Genevieve Schultz <genschultz@gmail.com>
2018-06-13 15:56:42 -0700

    cloud app finished

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 require('dotenv').config();
 var path = require('path');
 var ejsLayouts = require('express-ejs-layouts');
+var multer = require('multer');
+var upload = multer({dest: './uploads/'})
+var cloud = require('cloudinary');
 var app = express();
+var images = [];
 var port = process.env.PORT || 2000;
 
 app.set('view engine', 'ejs');
@@ -10,7 +14,7 @@ app.use(ejsLayouts);
 
 app.get('/', function (req, res) {
   console.log('get route hit');
-  res.render('index');
+  res.render('index', {arr: images, cloud});
 });
 
 app.get('/upload', function (req, res) {
@@ -18,9 +22,12 @@ app.get('/upload', function (req, res) {
   res.render('upload');
 });
 
-app.post('/upload', function (req, res) {
+app.post('/upload', upload.single('myFile'), function (req, res) {
   console.log('POST - upload hit');
-  res.send('You done good kid');
+  cloud.uploader.upload(req.file.path, function (result) {
+    images.push(result.public_id);
+    res.redirect('/');
+  });
 });
 
 app.listen(port, function () {
